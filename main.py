954fe0a18342a6984b1bb1eaa16f29a71b4ba5d8
Gabriel Rocha <120466775+gabrielrochass@users.noreply.github.com>
2024-03-14 22:29:38 -0300

    feat: area dos objetos

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import cv2
 from PIL import Image
 from util import getLimits
+# from util import calculaArea
+
+def calculaArea(bbox): # função que calcula a área de um retângulo
+    if bbox is not None:
+        x1, y1, x2, y2 = bbox
+        area = (x2 - x1) * (y2 - y1)
+        return area/216
+    else:
+        return 0
 
 # define limites de cor para cada cor em BGR
 yellow = [0, 255, 255] # amarelo em BGR
@@ -26,11 +35,18 @@ while True:
     if bboxYellow is not None:
         x1, y1, x2, y2 = bboxYellow
         frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 255), 5) # amarelo
+        area = calculaArea(bboxYellow)
+        area = "{:.2f}".format(area)
+        cv2.putText(frame, f"Area: {area} cm²", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
     bboxBlue = Image.fromarray(maskBlue).getbbox()
     if bboxBlue is not None:
         x1, y1, x2, y2 = bboxBlue
         frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 5) # azul
+        area = calculaArea(bboxBlue)
+        area = "{:.2f}".format(area)
+        cv2.putText(frame, f"Area: {area} cm²", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
 
     cv2.imshow("Color Detector", frame) # exibir o nome da janela e o frame capturado
     key = cv2.waitKey(1)
