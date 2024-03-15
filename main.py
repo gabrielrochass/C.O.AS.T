512af70618c9ef3119948667a5bfd3f0cc84f3dd
Gabriel Rocha <120466775+gabrielrochass@users.noreply.github.com>
2024-03-14 22:56:51 -0300

    feat: user escolhe a cor a ser detectada e cálculo da área ok

--- main.py
+++ main.py
@@ -1,58 +1,59 @@
 import cv2
 from PIL import Image
 from util import getLimits
-# from util import calculaArea
+from tkinter import Tk, colorchooser
 
 def calculaArea(bbox): # função que calcula a área de um retângulo
     if bbox is not None:
         x1, y1, x2, y2 = bbox
-        area = (x2 - x1) * (y2 - y1)
-        return area/216
+        largura_cm = (x2 - x1) * (2.54 / 96)  # 96 pixels por polegada (dpi), 2.54 cm por polegada
+        altura_cm = (y2 - y1) * (2.54 / 96)
+        area_cm2 = largura_cm * altura_cm
+        return area_cm2
     else:
         return 0
+    
+def getColor():
+    root = Tk()
+    root.withdraw()
+    color = colorchooser.askcolor(title="Selecione uma cor")
+    root.destroy()
+    if color[0] is not None:
+        return color[0]
+    else:
+        # se o usuário fechar o seletor de cor sem escolher uma, retorna none
+        return None
+
 
 # define limites de cor para cada cor em BGR
-yellow = [0, 255, 255] # amarelo em BGR
-blue = [255, 0, 0]     # azul em BGR
+color_rbg = getColor()
 
-cam = cv2.VideoCapture(0) # abrir a câmera
+if color_rbg is not None: # somente abre a câmera se o usuário escolher uma cor
+    color_bgr = [color_rbg[2], color_rbg[1], color_rbg[0]]
 
-while True:
-    ret, frame = cam.read() # capturar um frame da câmera
+    cam = cv2.VideoCapture(0) # abrir a câmera
 
-    hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) # converter o frame de BGR para HSV
+    while True:
+        ret, frame = cam.read() # capturar um frame da câmera
 
-    # detecção de cor para amarelo
-    lowerLimitYellow, upperLimitYellow = getLimits(yellow) 
-    maskYellow = cv2.inRange(hsvImage, lowerLimitYellow, upperLimitYellow) 
- 
-    # detecção de cor para azul
-    lowerLimitBlue, upperLimitBlue = getLimits(blue)
-    maskBlue = cv2.inRange(hsvImage, lowerLimitBlue, upperLimitBlue)
+        hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) # converter o frame de BGR para HSV
+        
+        lowerLimitColor, upperLimitColor = getLimits(color_bgr)
+        maskColor = cv2.inRange(hsvImage, lowerLimitColor, upperLimitColor)
 
-    # desenhar caixas limite para cada objeto detectado
-    bboxYellow = Image.fromarray(maskYellow).getbbox()
-    if bboxYellow is not None:
-        x1, y1, x2, y2 = bboxYellow
-        frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 255), 5) # amarelo
-        area = calculaArea(bboxYellow)
-        area = "{:.2f}".format(area)
-        cv2.putText(frame, f"Area: {area} cm²", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+        bboxColor = Image.fromarray(maskColor).getbbox()
+        if bboxColor is not None:
+            x1, y1, x2, y2 = bboxColor
+            frame = cv2.rectangle(frame, (x1, y1), (x2, y2), color_bgr, 5) # cor selecionada
+            area = calculaArea(bboxColor)
+            area = "{:.2f}".format(area)
+            cv2.putText(frame, f"Area: {area} cm**2", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
-    bboxBlue = Image.fromarray(maskBlue).getbbox()
-    if bboxBlue is not None:
-        x1, y1, x2, y2 = bboxBlue
-        frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 5) # azul
-        area = calculaArea(bboxBlue)
-        area = "{:.2f}".format(area)
-        cv2.putText(frame, f"Area: {area} cm²", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+        cv2.imshow("Color Detector", frame) # exibir o nome da janela e o frame capturado
+        key = cv2.waitKey(1)
 
+        if (key == 27) or key == ord('q') or cv2.getWindowProperty("Color Detector", cv2.WND_PROP_VISIBLE) < 1: # aguardar pressionamento da tecla 'q' para sair or esc or x da janela
+            break
 
-    cv2.imshow("Color Detector", frame) # exibir o nome da janela e o frame capturado
-    key = cv2.waitKey(1)
-
-    if (key == 27) or key == ord('q') or cv2.getWindowProperty("Color Detector", cv2.WND_PROP_VISIBLE) < 1: # aguardar pressionamento da tecla 'q' para sair or esc or x da janela
-        break
-
-cam.release() # liberar a câmera
-cv2.destroyAllWindows() # fechar todas as janelas abertas
+    cam.release() # liberar a câmera
+    cv2.destroyAllWindows() # fechar todas as janelas abertas
