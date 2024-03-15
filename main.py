d40a66f9df3529e64870b5f1bbaeddc75b567ab7
Gabriel Rocha <120466775+gabrielrochass@users.noreply.github.com>
2024-03-14 21:48:34 -0300

    feat: detecta azul com retangulo azul

--- main.py
+++ main.py
@@ -2,31 +2,41 @@ import cv2
 from PIL import Image
 from util import getLimits
 
-yellow = [0, 255, 255] # cor amarela em BGR
+# define limites de cor para cada cor em BGR
+yellow = [0, 255, 255] # amarelo em BGR
+blue = [255, 0, 0]     # azul em BGR
 
-cam = cv2.VideoCapture(0) # abre a câmera
+cam = cv2.VideoCapture(0) # abrir a câmera
 
 while True:
-    ret, frame = cam.read() # captura um frame da câmera
+    ret, frame = cam.read() # capturar um frame da câmera
 
-    hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) # converte o frame de BGR para HSV
+    hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) # converter o frame de BGR para HSV
 
-    lowerLimit, upperLimit = getLimits(yellow) # obtém os limites inferior e superior para a cor amarela
+    # detecção de cor para amarelo
+    lowerLimitYellow, upperLimitYellow = getLimits(yellow) 
+    maskYellow = cv2.inRange(hsvImage, lowerLimitYellow, upperLimitYellow) 
+ 
+    # detecção de cor para azul
+    lowerLimitBlue, upperLimitBlue = getLimits(blue)
+    maskBlue = cv2.inRange(hsvImage, lowerLimitBlue, upperLimitBlue)
 
-    mask = cv2.inRange(hsvImage, lowerLimit, upperLimit) # cria máscara para pixels de cor amarela
+    # desenhar caixas limite para cada objeto detectado
+    bboxYellow = Image.fromarray(maskYellow).getbbox()
+    if bboxYellow is not None:
+        x1, y1, x2, y2 = bboxYellow
+        frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 255), 5) # amarelo
 
-    fullMask = Image.fromarray(mask) # cria um objeto Image a partir da máscara
+    bboxBlue = Image.fromarray(maskBlue).getbbox()
+    if bboxBlue is not None:
+        x1, y1, x2, y2 = bboxBlue
+        frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 5) # azul
 
-    bbox = fullMask.getbbox() # obtém bounding box da máscara
+    cv2.imshow("Color Detector", frame) # exibir o nome da janela e o frame capturado
+    key = cv2.waitKey(1)
 
-    if bbox is not None: # se a bounding box não for vazia
-        x1, y1, x2, y2 = bbox
-        frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 5) 
-
-    # cv2.imshow("Color Detector", mask) -> deixa a tela toda preta e quando aparece a cor amarela, ela fica branca no lugar da cor amarela (objeto)
-    cv2.imshow("Color Detector", frame) # exibe o nome da janela e o frame capturado
-    if cv2.waitKey(1) & 0xFF == ord('q'): # aguarda pressionamento da tecla 'q' para sair
+    if (key == 27) or key == ord('q') or cv2.getWindowProperty("Color Detector", cv2.WND_PROP_VISIBLE) < 1: # aguardar pressionamento da tecla 'q' para sair or esc or x da janela
         break
 
-cam.release() # libera a câmera
-cv2.destroyAllWindows() # fecha todas as janelas abertas
+cam.release() # liberar a câmera
+cv2.destroyAllWindows() # fechar todas as janelas abertas
